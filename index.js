adad33431cc97f407d580d8c7c57c72092b646af
abdulhay103 <abdulhay.me@gmail.com>
2024-03-02 00:06:55 +0600

    good working

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
+var firstName;
+var lastName;
+var age;
+firstName = "Abdul";
+lastName = " Hay";
+var fullName = firstName.concat(lastName);
+console.log(fullName.split(" "));
 function addNumber(num1, num2) {
     console.log(num1 + num2);
 }
