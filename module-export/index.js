696489b2515f30568fed54b216593e08c8b4f731
abdulhay103 <abdulhay.me@gmail.com>
2024-03-06 10:04:00 +0600

    module done

---
+++ module-export/index.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var module_1 = require("./module");
+var user = new module_1.User("Abdul Hay", 30);
+user.display();
---
+++ module-export/index.ts
@@ -0,0 +1,5 @@
+import { User } from "./module";
+
+let user = new User("Abdul Hay", 30);
+
+user.display();
